55afdbe16aefdb3f6c2c2852cff845c2a7bc2647
Justway <986923254@qq.com>
2022-11-03 16:27:26 +0800

    nprogress进度条管理

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 import requests from "./request";
 
 // 三级联动接口
-// api/product/getBaseCategoryList  get  无参数
+// api/product/getBaseCategoryList  get  无参数  已失效
 export const reqCategoryList = () =>
   // 返回结果为Promise对象
   requests({
--- src/api/request.js
+++ src/api/request.js
@@ -1,22 +1,34 @@
 //对axios进行二次封装
-import axios from 'axios';
+import axios from "axios";
+// 引入进度条   start: 进度条开始    done: 进度条结束
+import nProgress from "nprogress";
+// 引入进度条样式
+import "nprogress/nprogress.css"
+
 
 //使用axios对象的方法create，去创建一个axios实例
 //request就是axios，只不过进行了二次封装
 const requests = axios.create({
-    //配置对象
-    baseURL:'/api',
-    timeout:5000
-})
+  //配置对象
+  baseURL: "/api",
+  timeout: 5000,
+});
 //请求拦截器
-requests.interceptors.request.use((config)=>{
-    //config:配置对象
-    return config
-})
+requests.interceptors.request.use((config) => {
+  //config:配置对象
+  // 进度条开始
+  nProgress.start()
+  return config;
+});
 //相应拦截器
-requests.interceptors.response.use((res)=>{
-    return res.data
-},(error)=>{
-    return Promise.reject(error)
-})
-export default requests
+requests.interceptors.response.use(
+  (res) => {
+    // 进度条结束
+    nProgress.done()
+    return res.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+export default requests;
