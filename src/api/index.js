b3e8aeb45dde20b7540c3ceac96bcf963140a61a
Justway <986923254@qq.com>
2022-11-03 16:14:44 +0800

    配置三级联动组件接口

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+// API接口进行统一管理
+import requests from "./request";
+
+// 三级联动接口
+// api/product/getBaseCategoryList  get  无参数
+export const reqCategoryList = () =>
+  // 返回结果为Promise对象
+  requests({
+    url: "/product/getBaseCategoryList",
+    method: "get",
+  });
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import App from './App.vue'
 import router from './router/index';
 //三级联动组件
 import TypeNavIndex from '@/pages/Home/TypeNavIndex';
-
+import {reqCategoryList} from '@/api/index'
 Vue.component(TypeNavIndex.name,TypeNavIndex)
 
 Vue.config.productionTip = false
-
+reqCategoryList()
 new Vue({
   render: h => h(App),
   router
